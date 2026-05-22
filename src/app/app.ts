f84c2933795cd154f412f7d3f7201027e3a38809
devdouanla <douanladevellop@gmail.com>
2026-05-22 20:45:11 +0100

    second commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,24 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
+import { SidebarComponent } from '../app/';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ToastComponent } from './components/toast/toast.component';
+
+import { LoginComponent } from './views/login/login.component';
+import { DashboardComponent } from './views/dashboard/dashboard.component';
+import { CompetitionsComponent } from './views/competitions/competitions.component';
+import { TeamsComponent } from './views/teams/teams.component';
+import { PlayersComponent } from './views/players/players.component';
+import { MatchesComponent } from './views/matches/matches.component';
+import { RankingsComponent } from './views/rankings/rankings.component';
+import { NotificationsComponent } from './views/notifications/notifications.component';
+import { ProfileComponent } from './views/profile/profile.component';
+import { AuditLogsComponent } from './views/audit-logs/audit-logs.component';
+
+import { AuthService } from './services/auth.service';
+import { NotificationService } from './services/notification.service';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
---
+++ src/app/components/sidebar/sidebar.ts
@@ -0,0 +1,95 @@
+import { CommonModule } from '@angular/common';
+
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+
+import { FormsModule } from '@angular/forms';
+import{
+  LucideAngularModule,
+  Trophy,
+  Users,
+  UserCheck,
+  Calendar,
+  Layers,
+  Bell,
+  User,
+  Shield,
+  LogOut,
+  Activity,
+  X
+} from 'lucide-angular';
+
+@Component({
+  selector: 'app-sidebar',
+  imports: [CommonModule,FormsModule,LucideAngularModule],
+  templateUrl: './sidebar.html',
+  styleUrl: './sidebar.css',
+})
+export class Sidebar {
+
+
+  @Input() currentRoute!: string;
+  @Input() currentUser: any =true;
+  @Input() unreadCount = 0;
+  @Input() isOpen = false;
+
+  navItems: any[] = [];
+
+  readonly Trophy = Trophy;
+  readonly Users = Users;
+  readonly UserCheck = UserCheck;
+  readonly Calendar = Calendar;
+  readonly Layers = Layers;
+  readonly Bell = Bell;
+  readonly UserIcon = User;
+  readonly Shield = Shield;
+  readonly LogOut = LogOut;
+  readonly Activity = Activity;
+  readonly X = X;
+
+  ngOnInit(): void {
+    if (!this.currentUser) return;
+
+    this.navItems = [
+      { id: 'dashboard', label: 'Tableau de bord', icon: this.Activity },
+      { id: 'competitions', label: 'Compétitions', icon: this.Trophy },
+      { id: 'equipes', label: 'Équipes IUSJ', icon: this.Users },
+      { id: 'joueurs', label: 'Joueurs', icon: this.UserCheck },
+      { id: 'matchs', label: 'Matchs & Calendrier', icon: this.Calendar },
+      { id: 'classements', label: 'Classements', icon: this.Layers },
+      {
+        id: 'notifications',
+        label: 'Notifications',
+        icon: this.Bell,
+        badge: this.unreadCount
+      },
+      { id: 'profile', label: 'Profil & Rôles', icon: this.UserIcon },
+    ];
+
+    if (
+      this.currentUser.role === 'Administrateur' ||
+      this.currentUser.role === 'Responsable sportif'
+    ) {
+      this.navItems.push({
+        id: 'audit-logs',
+        label: 'Journal de Sécurité',
+        icon: this.Shield
+      });
+    }
+  }
+
+  onNavigate(route: string): void {
+    console.log('Navigate to:', route);
+
+    if (window.innerWidth < 1024) {
+      this.onToggle();
+    }
+  }
+
+  onLogout(): void {
+    console.log('Logout');
+  }
+
+  onToggle(): void {
+    this.isOpen = !this.isOpen;
+  }
+}
